436f69efc3dd6d43994bbbf66cd05bcd858bf96c
01m1 <osimayungbo@gmail.com>
2025-06-06 17:30:15 +0100

    Time 0-byte read

---
+++ LimitedDirectExecution/syscallmeasure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+// Measuring the cost of a system call
+
+int main() {
+    struct timeval current_time;
+    struct timeval next_time;
+    gettimeofday(&current_time, NULL);
+    read(0, NULL, 0);
+    gettimeofday(&next_time, NULL);
+    printf("Took %ld microseconds\n", next_time.tv_usec-current_time.tv_usec);
+
+    return 0;
+}
