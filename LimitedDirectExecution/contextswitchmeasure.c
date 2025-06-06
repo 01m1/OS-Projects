cf2cffd9300edec0508db1761f269198413804fa
01m1 <osimayungbo@gmail.com>
2025-06-06 18:12:05 +0100

    Set process to run on a single CPU

--- LimitedDirectExecution/contextswitchmeasure.c
+++ LimitedDirectExecution/contextswitchmeasure.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <sched.h>
 
 // Measuring the cost of a system call
 
 int main() {
 
-
+    cpu_set_t my_set; // Define your cpu_set bit mask.
+    CPU_ZERO(&my_set); // Initialize it all to 0, i.e. no CPUs selected.
+    CPU_SET(7, &my_set); // set the bit that represents core 7.
+    sched_setaffinity(0, sizeof(cpu_set_t), &my_set); // Set affinity of tihs process to the defined mask, i.e. only 7.
     return 0;
 }
