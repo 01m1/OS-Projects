25ce7692a3316d92349e76fca8c0489dac03d9e9
01m1 <osimayungbo@gmail.com>
2025-07-03 00:01:03 +0100

    Add illegal array access

--- VirtualMemory/null.c
+++ VirtualMemory/null.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    /*
     int x = 5;
     int* p = &x;
     p = NULL;
     int a = *p;
+    */
 
+    int* x = malloc(sizeof(int)*100);
+    x[100] = 0;
+    
     return 0;
 }
